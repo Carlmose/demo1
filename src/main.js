4ceec3a1695b0b65c8f8eeff4565330f9f0f0939
carlmose <mozhongyim@gmial.com>
2024-11-07 18:09:35 +0800

    表单验证学习

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// 引入 Element UI 样式和组件
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
